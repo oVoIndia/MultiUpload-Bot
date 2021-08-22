1ce408dacd9b8d1f7725c19341940d2e4f52c9e8
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2021-08-22 12:52:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,4 @@ class Config(object):
    API_HASH = os.environ.get("API_HASH")
    # get a token from @BotFather
    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-   # channel id = -100 (for logs)
-   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))   
-   # Array to store users who are authorized to use the bot 
-   AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())    
-   # force sub user to the channel (without @)
-   CHNAME = os.environ.get("CHNAME")
+   
