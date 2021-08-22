13d25e415c2e9edf10917d38823c63d90ff31ffe
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2021-08-22 12:45:19 +0530

    Update tsh.py

--- multiupload/plugins/tsh.py
+++ multiupload/plugins/tsh.py
@@ -6,15 +6,12 @@ import asyncio, os, requests, time
 from requests import post
 from multiupload import anjana
 from telethon.sync import events, Button
-from multiupload.fsub import *
 from multiupload.utils import downloader, humanbytes
 from config import Config
 
 @anjana.on(events.NewMessage(pattern='^/tsh'))
 async def transfer(event):
 	user_id = event.sender_id
-	if event.is_private and not await check_participant(user_id, f'@{Config.CHNAME}', event):
-		return
 	if event.reply_to_msg_id:
 		pass
 	else:
@@ -26,14 +23,7 @@ async def transfer(event):
 	amjana = await event.get_reply_message()
 
 
-	## LOGGING TO A CHANNEL
-	xx = await event.get_chat()
-	reqmsg = f'''Req User: [{xx.first_name}](tg://user?id={xx.id})
-FileName: {amjana.file.name}
-FileSize: {humanbytes(amjana.file.size)}
-#TRANSFERSH'''
-	await anjana.send_message(Config.LOG_CHANNEL, reqmsg)
-
+	##UPLOADING...
 	result = await downloader(
 		f"downloads/{amjana.file.name}",
 		amjana.media.document,
