0e623ddd1f94288bfdb836c9b87090f3dbab6e60
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2021-08-22 13:00:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,15 @@ class Config(object):
    API_HASH = os.environ.get("API_HASH")
    # get a token from @BotFather
    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-   
+   HELPMSG = '''
+➖ **Help Menu | MultiUpload Bot**➖
+● `/gofile` - Upload files to GoFile
+● `/anonfile` - Upload files to AnonFile
+● `/ufile` - Upload files to UFile
+● `/bayfiles` - Upload files to BayFiles
+● `/tsh` - Upload files to TransferSH
+● `/tninja` - Upload files to TmNinja
+● `/fileio` - Upload files to FileIO
+● `/mixdrop` - Upload files to MixDrop
+✦ **Powered By [oVoIndia]**(https://github.com/oVoIndia)
+✦ Made with ♥️ by [HxBots](t.me/hxbots)'''
