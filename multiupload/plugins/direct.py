c8ed0b8bc8ddda44d989acedb1b6c9d3099b35ae
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2021-08-25 20:07:11 +0530

    Create direct.py

---
+++ multiupload/plugins/direct.py
@@ -0,0 +1,53 @@
+
+import asyncio, os, requests, time
+from requests import post
+from multiupload import anjana
+from telethon.sync import events, Button
+from multiupload.fsub import *
+from multiupload.utils import downloader, humanbytes
+from config import Config
+
+
+
+@anjana.on(events.NewMessage(pattern='^/direct'))
+async def transfer(event):
+	user_id = event.sender_id
+	if event.is_private and not await check_participant(user_id, f'@{Config.CHNAME}', event):
+		return
+	if event.reply_to_msg_id:
+		pass
+	else:
+		return await event.edit("Please Reply to File")
+
+	async with anjana.action(event.chat_id, 'typing'):
+		await asyncio.sleep(2)
+	msg = await event.reply("**Processing...**")
+	amjana = await event.get_reply_message()
+    try:
+        log_msg = await m.forward(chat_id=Config.LOG_CHANNEL)
+        stream_link = "https://{}/{}".format(Config.APP_NAME, log_msg.message_id)) else \
+            "http://{}:{}/{}".format(Config.APP_NAME,
+                                    Config.PORT,
+                                    log_msg.message_id)
+        file_size = None
+        if m.video:
+            file_size = f"{humanbytes(m.video.file_size)}"
+        elif m.document:
+            file_size = f"{humanbytes(m.document.file_size)}"
+        elif m.audio:
+            file_size = f"{humanbytes(m.audio.file_size)}"
+
+        file_name = None
+        if m.video:
+            file_name = f"{m.video.file_name}"
+        elif m.document:
+            file_name = f"{m.document.file_name}"
+        elif m.audio:
+            file_name = f"{m.audio.file_name}"
+
+        await msg.edit(hmm, buttons=(
+		[Button.url('📦 Download', {stream_link)],
+		[Button.url('Support Chat 💭', 't.me/hxsupport')]
+		))
+
+	os.remove(result.name)
