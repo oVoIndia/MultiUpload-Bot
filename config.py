a29431d1ae087aed461da7c1f425d6f483be575a
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2022-08-12 08:47:19 +0530

    Update config.py
    move some config's to optional

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ class Config(object):
    # get a token from @BotFather
    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
    # channel id = -100 (for logs)
-   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))   
+   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001378693486"))   
    # Array to store users who are authorized to use the bot 
-   AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())    
+   AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "754495556").split())    
    # force sub user to the channel (without @)
-   CHNAME = os.environ.get("CHNAME")
+   CHNAME = os.environ.get("CHNAME", "hxbots")
